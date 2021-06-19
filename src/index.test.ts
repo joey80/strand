c56948863a18561e85b37fc1be3ab923b7d7dad2
Joey <legerinvestments@gmail.com>
2021-06-19 16:16:04 -0400

    started stubbing out new methods

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,19 @@
-import { test } from './index';
+import { test } from './index'
+import { limitChars } from './methods'
 
 describe('test', () => {
   it('should return the correct string', () => {
-    const assert = test('joey');
+    const assert = test('joey')
+    expect(assert).toBe('joey is a string')
+  })
 
-    expect(assert).toBe('joey is a string');
-  });
-});
+  it('should limit chars', () => {
+    const assert = limitChars('this is something', 7)
+    expect(assert).toBe('this is')
+  })
+
+  it('should limit chars with an ellipse', () => {
+    const assert = limitChars('this is something', 7, '...')
+    expect(assert).toBe('this is ...')
+  })
+})
