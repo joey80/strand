575f12ef51346cd800efc476f615527894069ec2
Joey <legerinvestments@gmail.com>
2021-06-19 22:09:05 -0400

    feat: added Strand class

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,66 @@ import {
   titleCase,
 } from './methods'
 
+class Strand {
+  private string: string
+  static chain(str: string) {
+    return new Strand(str)
+  }
+
+  constructor(str: string) {
+    this.string = str
+  }
+
+  camelCase() {
+    this.string = camelCase(this.string)
+    return this
+  }
+
+  get() {
+    return this.string
+  }
+
+  kebabCase() {
+    this.string = kebabCase(this.string)
+    return this
+  }
+
+  limitChars(limit: number, append = '') {
+    this.string = limitChars(this.string, limit, append)
+    return this
+  }
+
+  limitWords(limit: number, append = '') {
+    this.string = limitWords(this.string, limit, append)
+    return this
+  }
+
+  pascalCase() {
+    this.string = pascalCase(this.string)
+    return this
+  }
+
+  removeWhitespace() {
+    this.string = removeWhitespace(this.string)
+    return this
+  }
+
+  snakeCase() {
+    this.string = snakeCase(this.string)
+    return this
+  }
+
+  titleCase() {
+    this.string = titleCase(this.string)
+    return this
+  }
+}
+
+const { chain } = Strand
+
 export {
   camelCase,
+  chain,
   kebabCase,
   limitChars,
   limitWords,
