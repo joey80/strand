650a250b27ae0ca4aa9b6c7a76de128ad9cc7fe3
Joey <legerinvestments@gmail.com>
2021-06-19 21:36:01 -0400

    feat: added camelCase, kebabCase, limitChars, limitWords, pascalCase, removeWhitespace, snakeCase, titleCase

--- src/methods/index.ts
+++ src/methods/index.ts
@@ -1,16 +1,42 @@
-const camelCase = (str: string) => ''
+import { capitalizeWord } from '../utils'
+
+const camelCase = (str: string) =>
+  str
+    .split(' ')
+    .map((elm, index) => (index === 0 ? elm.toLowerCase() : capitalizeWord(elm)))
+    .join('')
+
+const kebabCase = (str: string) => str.split(' ').join('-')
 
 const limitChars = (str: string, limit: number, append = '') =>
   `${str.substring(0, limit)} ${append}`.trim()
 
-const limitWords = (str: string, limit: number, append = '') => ''
+const limitWords = (str: string, limit: number, append = '') =>
+  `${str.split(' ').slice(0, limit).join(' ')} ${append}`.trim()
 
-const pascalCase = (str: string) => ''
+const pascalCase = (str: string) =>
+  str
+    .split(' ')
+    .map((elm) => capitalizeWord(elm))
+    .join('')
 
-const removeWhitespace = (str: string) => ''
+const removeWhitespace = (str: string) => str.replace(/\s(?=\s)/g, '').trim()
 
-const snakeCase = (str: string) => ''
+const snakeCase = (str: string) => str.split(' ').join('_')
 
-const titleCase = (str: string) => ''
+const titleCase = (str: string) =>
+  str
+    .split(' ')
+    .map((elm) => capitalizeWord(elm))
+    .join(' ')
 
-export { camelCase, limitChars, limitWords, pascalCase, removeWhitespace, snakeCase, titleCase }
+export {
+  camelCase,
+  kebabCase,
+  limitChars,
+  limitWords,
+  pascalCase,
+  removeWhitespace,
+  snakeCase,
+  titleCase,
+}
